3335d35708ef00bea68383a61e5d6ec25113e45b
noclips-fire <shevelovdmytro2006@gmail.com>
2025-04-03 10:10:17 +0200

    update 1.8

--- EventManager/resources/views/events/edit.blade.php
+++ EventManager/resources/views/events/edit.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 <body>
     <h1>Update an Event</h1>
